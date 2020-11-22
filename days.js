de3b2664117825e28959eb13e28795b3b641650b
clearxlate <73571290+clearxlate@users.noreply.github.com>
2020-11-21 23:12:21 -0500

    sep js

--- days.js
+++ days.js
@@ -1,9 +1,4 @@
-funtion makeDays() {
-	dt1 = new Date("08/02/2020");
-	dt2 = new Date();
-	this = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
-	document.write(this);
-	
-//document.getElementById("demo").innerHTML = toCelsius(77);
-
-document.write("<h1>asdf</h1>");
+dt1 = new Date("08/02/2020");
+dt2 = new Date();
+Result = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+document.getElementById("iddays").innerHTML = "<h1>" + Result + "</h1>";
