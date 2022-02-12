89a39663fd2307291090636c33a21363d09b91d8
clearxlate <73571290+clearxlate@users.noreply.github.com>
2022-02-12 18:41:42 -0500

    Title

--- days.js
+++ days.js
@@ -2,3 +2,4 @@ dt1 = new Date("08/02/2020");
 dt2 = new Date();
 Result = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
 document.getElementById("iddays").innerHTML = Result;
+document.title = Result;
