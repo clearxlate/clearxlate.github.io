4ff9cac5ef8b13895b5520dd0b467ae4c01ef49c
clearxlate <clearxlate@gmail.com>
2022-10-31 03:13:00 -0400

    Not Empty

--- days.js
+++ days.js
@@ -1,5 +1,4 @@
-dt1 = new Date("08/02/2020");
-dt2 = new Date();
-Result = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
-document.getElementById("iddays").innerHTML = Result;
-document.title = Result;
+var dt1 = new Date("08/02/2020");
+var dt2 = new Date();
+var Result = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+console.log(Result)
